d7cdc698c0be35d3e984b4fde4ec012d9f640ae5
DESKTOP-IA41EAO\CORBA <marti.corbalan@mataro.epiaedu.cat>
2020-11-23 20:56:22 +0100

    hem falla al connectar amb firebase

--- app/src/main/java/com/example/matematiques/registrarse.java
+++ app/src/main/java/com/example/matematiques/registrarse.java
@@ -1,48 +1,91 @@
 package com.example.matematiques;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class registrarse extends AppCompatActivity {
+    private static final String TAG = null;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     EditText nom;
     EditText email;
     EditText contrasenya;
 
 
+
+    String username;
+    String gmail;
+    String pwd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
 
-        nom=(EditText)findViewById(R.id.username);
+        nom= findViewById(R.id.username);
         email=findViewById(R.id.email);
         contrasenya=findViewById(R.id.password);
 
-        Button btnlogin = (Button) findViewById(R.id.botonlogin);
-        final EditText usuari = (EditText) findViewById(R.id.username);
+        Button btnlogin =  findViewById(R.id.botonlogin);
+        //final EditText usuari = (EditText) findViewById(R.id.username);
+
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nom=usuari.getText().toString();
+                 username = nom.getText().toString();
+                gmail = email.getText().toString();
+                pwd = contrasenya.getText().toString();
+                Map<String, Object> user = new HashMap<>();
+
+                user.put("usuari", username);
+                user.put("email", gmail);
+                user.put("password", pwd);
+
+// Add a new document with a generated ID
+                db.collection("usuari")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
 
                 Intent intent = new Intent(registrarse.this, MainActivity.class);
                 Bundle b = new Bundle();
-                b.putString ("usuaris",nom);
+                b.putString ("usuaris", String.valueOf(nom));
                 intent.putExtras(b);
                 startActivity(intent);
+
+
 
             }
 
 
         });
-
 
 
 
